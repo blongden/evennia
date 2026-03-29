a9a851677bad7562541ac9ea5f2b903c608d8193
Ben Longden <ben@longden.me>
2026-03-29 18:49:39 +0100

    Debug: check which nomatch cmd is returned

--- evennia/commands/cmdhandler.py
+++ evennia/commands/cmdhandler.py
@@ -702,7 +702,7 @@ def cmdhandler(
                 _getinput = getattr(getattr(caller, "ndb", None), "_getinput", None)
                 if _getinput:
                     syscmd = yield cmdset.get(CMD_NOMATCH)
-                    logger.log_info(f"[DEBUG] get_input active. CMD_NOMATCH={syscmd}. cmdset keys={[c.key for c in cmdset.commands]}. cmdset prio={cmdset.priority} merge={cmdset.mergetype}")
+                    logger.log_info(f"[DEBUG] get_input active. CMD_NOMATCH={syscmd} type={type(syscmd).__name__} from_cmdset={getattr(syscmd, 'from_cmdset', '?')}")
                     if syscmd:
                         syscmd.raw_string = unformatted_raw_string
                         raise ExecSystemCommand(syscmd, unformatted_raw_string)
